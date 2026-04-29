9db1f13e798cfa0baaed248bd06c6fedf4b59401
ilyes <i.belkhiri@esi-sba.dz>
2026-04-29 11:59:52 +0100

    raouf sni9cho

--- medical_records/serializers.py
+++ medical_records/serializers.py
@@ -1,30 +1,36 @@
 from rest_framework import serializers
 from .models import MedicalRecord, Consultation, Diagnosis, Medication, Prescription, Attachment
 
+
 class MedicalRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalRecord
         fields = '__all__'
+
 
 class ConsultationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Consultation
         fields = '__all__'
 
+
 class DiagnosisSerializer(serializers.ModelSerializer):
     class Meta:
         model = Diagnosis
         fields = '__all__'
+
 
 class MedicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medication
         fields = '__all__'
 
+
 class PrescriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prescription
         fields = '__all__'
+
 
 class AttachmentSerializer(serializers.ModelSerializer):
     class Meta:
